e3a29cbc72929f2ec3c83388cd5e8444d8219122
Izaias2008 <42851714+Izaias2008@users.noreply.github.com>
2018-08-30 20:16:51 -0300

    Add files via upload

---
+++ AulaGITHUB/src/aulagithub/AulaGITHUB.java
@@ -0,0 +1,49 @@
+
+package aulagithub;
+
+import java.util.Scanner;
+
+
+public class AulaGITHUB {
+
+    public static void main(String[] args) {
+     Scanner in = new Scanner (System.in);
+        System.out.println("digite um numero de 1 a 12 "
+                + "sendo que 1 para janeiro e 12 para dezembo"); 
+int mes;
+mes = in.nextInt();
+switch (mes){
+    case 1:
+        System.out.println("jeneiro");
+         case 2:
+        System.out.println("fevereiro");
+        case 3 :
+        System.out.println("março");
+        case 4 :
+        System.out.println("abril");
+        case 5:
+        System.out.println("maio");
+        case 6:
+        System.out.println("junho");
+        case 7:
+        System.out.println("julio");
+        case 8:
+        System.out.println("agosto");
+        case 9:
+        System.out.println("setembro");
+        case 10:
+        System.out.println("outubro");
+        case 11:
+        System.out.println("novembro");
+        case 12:
+        System.out.println("dezembro");
+        
+        
+}
+    
+
+
+
+    }
+    
+}
